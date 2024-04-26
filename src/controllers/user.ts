f09400663e80bd912d9ae265e867c10c5c3b8474
imdadulhaque1 <imdadulhaqueimdad820@gmail.com>
2024-04-26 11:48:35 +0600

    Refactor code for mailTrap

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,39 +1,17 @@
 import { RequestHandler } from "express";
-import nodemailer from "nodemailer";
-
 import { createUser } from "#/@types/user";
-import EmailVerificationToken from "#/models/emailVerificationToken";
 import User from "#/models/user";
 import { generateToken } from "#/utils/helper";
-import { MAILTRAP_PASS, MAILTRAP_USER } from "#/utils/variables";
+import { sendVerificationmail } from "#/utils/mail";
 
 export const create: RequestHandler = async (req: createUser, res) => {
   const { email, password, name } = req.body;
 
   const user = await User.create({ email, password, name });
 
-  const transport = nodemailer.createTransport({
-    host: "sandbox.smtp.mailtrap.io",
-    port: 2525,
-    auth: {
-      user: MAILTRAP_USER,
-      pass: MAILTRAP_PASS,
-    },
-  });
-
+  // send verification Email
   const token = generateToken();
-  await EmailVerificationToken.create({
-    owner: user._id,
-    token,
-  });
+  sendVerificationmail(token, { name, email, userId: user._id.toString() });
 
-  console.log("Token: ", token);
-
-  transport.sendMail({
-    to: user.email,
-    from: "imdadulhaque1440@gmail.com",
-    html: `Your verification token is: ${token}`,
-  });
-
-  res.status(201).json({ user });
+  res.status(201).json({ user: { id: user._id, name, email } });
 };
